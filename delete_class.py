cd017ed5a9e08df4c9fb20273c6ed3b1f93b213a
kawanishi <56061392+m9-kawanishi@users.noreply.github.com>
2023-03-06 14:49:57 +0900

    first commit

---
+++ delete_class.py
@@ -0,0 +1,47 @@
+import numpy as np
+import os
+
+###############################################################
+
+# ディレクトリ設定
+directory = "/content/a/"
+
+################################################################
+
+
+
+for file in os.listdir(directory):
+    base, ext = os.path.splitext(file)
+    # txtファイルだけ抽出
+    if ext == '.txt':
+        fileName = directory + file
+
+    # ファイルオープン
+    with open(fileName, encoding="cp932") as f:
+        annotation = f.read().split( )  # 空白で分割しリストに保存
+
+    # 数値に変換
+    annotation = [float(num) for num in annotation]
+    annotation = np.array(annotation).reshape(-1,5)
+    count = 0
+  
+        
+    for i in range(annotation.shape[0]):
+        try:
+          if np.count_nonzero(annotation[i][0] == 80) > 0:
+              annotation = np.delete(annotation, i, 0)
+        except:
+          print("A")
+
+    np.savetxt(fileName, annotation, fmt ='%.6f')
+
+    with open(fileName, encoding="cp932") as f:
+        annotation = f.read()
+
+    # float -> int
+    annotation = annotation.replace(".000000 ", " ")
+
+    with open(fileName, mode="w", encoding="cp932") as f:
+        f.write(annotation)
+
+print("finish!")
